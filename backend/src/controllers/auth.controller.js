5005065d164abab2d5189a07eaee81e2b321a6af
sanjit <sanjit.est14@gmail.com>
2026-03-17 09:58:46 +0600

    Basic API for Auth

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req, res) => {
+  res.send("SignUp Routes");
+}
+
+export const login = (req, res) => {
+  res.send("Login Routes");
+}
+
+export const logout = (req, res) => {
+  res.send("Logout Routes");
+}
